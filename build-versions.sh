de17e7c1f6297ef3637d436a1d457e2fe73e9b00
Aleks Rutins <aleks@rutins.com>
2021-11-14 09:58:03 -0500

    Add -i to sed, naming fix, no more inchworm

--- build-versions.sh
+++ build-versions.sh
@@ -5,13 +5,13 @@ function build() {
     mv TeamCode/build/outputs/apk/debug/TeamCode-debug.apk TeamCode-debug-$1.apk
     echo "Build release $1"
     ./gradlew assembleRelease
-    mv TeamCode/build/outputs/apk/debug/TeamCode-release.apk TeamCode-release-$1.apk
+    mv TeamCode/build/outputs/apk/release/TeamCode-release.apk TeamCode-release-$1.apk
 }
 # Mecanum drive base
 build mecanum
 # Programming board
-sed s/MecanumDriveBase/ProgrammingBoardDriveBase/ {TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics.java,TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MMMFreightFrenzyOpMode.java,TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MMMUltimateGoalOpMode.java}
+sed -i s/MecanumDriveBase/ProgrammingBoardDriveBase/ {TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics.java,TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MMMFreightFrenzyOpMode.java,TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MMMUltimateGoalOpMode.java}
 build programming-board
 # Inchworm mecanum
-sed s/ProgrammingBoardDriveBase/InchwormMecanumDriveBase/ {TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics.java,TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MMMFreightFrenzyOpMode.java,TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MMMUltimateGoalOpMode.java}
-build inchworm
+# sed -i s/ProgrammingBoardDriveBase/InchwormMecanumDriveBase/ {TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics.java,TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MMMFreightFrenzyOpMode.java,TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MMMUltimateGoalOpMode.java}
+# build inchworm
