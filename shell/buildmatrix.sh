e859a78447d4075b9b3322fa08b6cc66ea576c75
Aleks Rutins <aleks@rutins.com>
2022-01-15 15:39:22 -0500

    Use temporary file for sed

--- shell/buildmatrix.sh
+++ shell/buildmatrix.sh
@@ -3,16 +3,18 @@ matrix_priv_file=""
 function matrix/priv/build() {
     echo "Build debug $1"
     ./gradlew assembleDebug
-    mv TeamCode/build/outputs/apk/debug/TeamCode-debug.apk TeamCode-debug-$1.apk
+    mv TeamCode/build/outputs/apk/debug/TeamCode-debug.apk TeamCode-debug-"$1".apk
     echo "Build release $1"
     ./gradlew assembleRelease
-    mv TeamCode/build/outputs/apk/release/TeamCode-release.apk TeamCode-release-$1.apk
+    mv TeamCode/build/outputs/apk/release/TeamCode-release.apk TeamCode-release-"$1".apk
 }
 
 function matrix/priv/config() {
   echo "Using configuration $1"
-  echo $matrix_priv_file
-  sed -i .prev_config "s/RobotHardware CURRENT = .*;/RobotHardware CURRENT = $1;/" "$matrix_priv_file"
+  local tmp_config=$(mktemp)
+  sed "s/RobotHardware CURRENT = .*;/RobotHardware CURRENT = $1;/" "$matrix_priv_file" > "$tmp_config"
+  cat "$tmp_config" > "$matrix_priv_file"
+  rm "$tmp_config"
 }
 
 function matrix/build() {
