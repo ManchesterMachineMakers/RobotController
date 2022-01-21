01feca573c1fc3422b97b38675d9db8494ebf965
Aleks Rutins <aleks@rutins.com>
2022-01-21 17:40:46 -0500

    Add TeleOp annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics_GamepadOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics_GamepadOnly.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.diagnostics.Runner;
 import org.firstinspires.ftc.teamcode.diagnostics.tests.GamepadTest;
 import org.firstinspires.ftc.teamcode.diagnostics.util.DiagnosticsOpMode;
@@ -7,6 +9,7 @@ import org.firstinspires.ftc.teamcode.diagnostics.util.Testable;
 import org.firstinspires.ftc.teamcode.subassemblies.Gamepad;
 import org.firstinspires.ftc.teamcode.util.RobotConfig;
 
+@TeleOp(name = "Diagnostics (Gamepad Only)")
 public class Diagnostics_GamepadOnly extends DiagnosticsOpMode {
     @Override
     protected void tests() {
