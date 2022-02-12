c009f76192c9789ed7f49b41dfe5ddb64c868829
orvis-ec-pr <rutinsp@orvis.com>
2022-02-12 18:21:47 -0500

    Adding Freight Frenzy Expanded Programming Board configuration names.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Names_FreightFrenzyProgrammingBoard.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class Names_FreightFrenzyProgrammingBoard {
+    public static final String imu = "imu";
+
+    public static final String servo_DeliveryDoor = "servo1";
+    public static final String servo_DeliveryChuteLeft = "servo1";
+    public static final String servo_DeliveryChuteRight = "servo1";
+    public static final String servo_BlinkinLED =  "servo1";
+
+    public static final String motor_LeftFront = "left_front";
+    public static final String motor_RightFront = "right_front";
+    public static final String motor_LeftRear = "left_front";
+    public static final String motor_RightRear = "right_front";
+    public static final String motor_Intake = "intake1";
+    public static final String motor_DeliveryMotor = "delivery1";
+    // public static final String motor_DuckySpinner = "duckySpinner1";
+
+    public static final String range_Front = "left";
+    //public static final String range_Rear = "rear";
+    public static final String range_Left = "left";
+    public static final String range_Right = "right";
+
+    public static final String camera_Default = "Webcam 1";
+
+    public static final String delivery_Gamepad = "gamepad2";
+    public static final String intake_Gamepad = "gamepad2";
+    public static final String drivebase_Gamepad = "gamepad1";
+}
