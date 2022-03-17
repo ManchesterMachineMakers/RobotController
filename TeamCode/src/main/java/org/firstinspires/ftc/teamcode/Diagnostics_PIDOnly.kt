5efba4391ac4259e2ca5fef84b12f722e0d9df3b
Aleks Rūtiņš <aleks@rutins.com>
2022-03-17 19:21:47 -0400

    Add teleop annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics_PIDOnly.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics_PIDOnly.kt
@@ -4,7 +4,9 @@ import org.firstinspires.ftc.teamcode.diagnostics.util.DiagnosticsOpMode
 import org.firstinspires.ftc.teamcode.diagnostics.util.Testable
 import org.firstinspires.ftc.teamcode.diagnostics.Runner
 import org.firstinspires.ftc.teamcode.diagnostics.tests.PIDTest
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 
+@TeleOp(name = "PID Controller Diagnostic")
 class Diagnostics_PIDOnly : DiagnosticsOpMode() {
     override fun provides() = arrayOf<Testable>()
     override fun runTests(runner: Runner?) {
