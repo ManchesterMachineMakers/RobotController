52eb3d2b50619a975d1c9b9b48d6bf5e1a346750
orvis-ec-pr <rutinsp@orvis.com>
2022-03-18 13:38:47 -0400

    slide retract warning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyAutonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyAutonomous.kt
@@ -58,12 +58,11 @@ open class FreightFrenzyAutonomous(private val alliance: Alliance) : MMMFreightF
         val delivery = getHardware<Delivery>()
         val pathfinder = getHardware<Pathfinder>()
 
-        telemetry.speak("Important! Please retract the slides completely to zero before running this op mode!")
-        this.idle()
-        telemetry.speak("Repeat: Please retract the slides completely to zero before running this op mode!")
-        this.idle()
         telemetry.addLine("Slides are now AT ZERO.  If they are not FULLY RETRACTED, you will break them!  If they are not retracted, stop this OpMode, retract the slides, and restart.")
         telemetry.update()
+        this.idle()
+        telemetry.speak("Warning! Please retract the slides completely to zero before running this op mode!")
+        this.idle()
 
         waitForStart()
         log("Getting recognitions")
