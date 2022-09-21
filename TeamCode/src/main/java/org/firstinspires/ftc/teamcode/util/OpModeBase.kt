e9345a9236c27a153d58fb23e02c06aeaad31853
Aleks Rutins <aleks@rutins.com>
2022-09-21 17:50:37 -0400

    Fix CI

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/OpModeBase.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/OpModeBase.kt
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.util
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+// DO NOT REMOVE
+import org.firstinspires.ftc.teamcode.util.configurations.fullRobot
 import org.firstinspires.ftc.teamcode.util.configurations.programmingBoard
 
 abstract class OpModeBase : LinearOpMode() {
     override fun runOpMode() {
-        RobotConfig.activate(hardwareMap, ::programmingBoard)
+        RobotConfig.activate(hardwareMap, ::fullRobot)
         run()
     }
 
