14b7854090c696ece74d14dcfc6e3e9198cfea0a
Manchester Machine Makers <ftc16221@gmail.com>
2022-10-19 18:06:22 -0400

    Fix the teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoNotBreakThisTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoNotBreakThisTeleOp.java
@@ -33,15 +33,15 @@ public class DoNotBreakThisTeleOp extends LinearOpMode {
         if(opModeIsActive()) {
             while (opModeIsActive()) {
                 double r = Math.hypot(-gamepad1.left_stick_x, -gamepad1.left_stick_y);
-                double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x) - (Math.PI / 2);
+                double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
                 float rightX = gamepad1.right_stick_x;
                 double v1 = r * Math.cos(robotAngle) + rightX;
                 double v2 = r * Math.sin(robotAngle) - rightX;
                 double v3 = r * Math.sin(robotAngle) + rightX;
                 double v4 = r * Math.cos(robotAngle) - rightX;
-                leftFront.setPower(-v1);
+                leftFront.setPower(v1);
                 rightFront.setPower(v2);
-                leftRear.setPower(-v3);
+                leftRear.setPower(v3);
                 rightRear.setPower(v4);
             }
         }
