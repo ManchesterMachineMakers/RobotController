a7cd90ee3327a862367c59ba115965b78710b1e6
Aleks Rutins <aleks@rutins.com>
2022-11-30 17:45:46 -0500

    Add an opmode because people told me to

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HalfServo.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Initialize Servo to 0.5", group = "Random Bits")
+public class HalfServo extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Servo servo = hardwareMap.servo.get("initializeServo");
+        waitForStart();
+        servo.setPosition(0.5);
+    }
+}
