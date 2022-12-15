aa873976fa5474ca6d87851a29f26cacbb8ec817
Aleks Rūtiņš <aleks@rutins.com>
2022-12-14 19:00:24 -0500

    Use a set

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GamepadManager.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GamepadManager.kt
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.util
 import com.qualcomm.robotcore.hardware.Gamepad
 
 class GamepadManager(val gamepad: Gamepad) {
-    var wasPressed = mutableListOf<String>()
+    var wasPressed = mutableSetOf<String>()
     private fun getButton(name: String) = Gamepad::class.java.getDeclaredField(name).getBoolean(gamepad)
     fun on(button: String, closure: () -> Unit) {
         if(getButton(button)) {
