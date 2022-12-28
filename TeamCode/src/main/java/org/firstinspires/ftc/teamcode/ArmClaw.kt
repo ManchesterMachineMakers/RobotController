3a73d8cc6bc94341267675c8fd038c6f4e8a4cc9
Aleks Rūtiņš <aleks@rutins.com>
2022-12-28 09:25:12 -0500

    Implement arm claw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmClaw.kt
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode
+import com.qualcomm.robotcore.hardware.Servo
+import org.firstinspires.ftc.robotcore.external.ExportToBlocks
+import org.firstinspires.ftc.teamcode.util.CustomBlocksOpModeCompanion
+
+object ArmClaw : CustomBlocksOpModeCompanion() {
+    private lateinit var clawServo: Servo
+    private lateinit var wristServo: Servo
+
+    override fun exists()
+        = hardwareMap.servo.contains("clawServo") && hardwareMap.servo.contains("wristServo")
+
+    @JvmStatic
+    fun initHardware() {
+        clawServo = hardwareMap.servo.get("clawServo")
+        wristServo = hardwareMap.servo.get("wristServo")
+        wristServo.scaleRange(0.22, 0.4)
+    }
+
+    @JvmStatic @ExportToBlocks
+    fun open() {
+        clawServo.position = 0.5
+    }
+
+    @JvmStatic @ExportToBlocks
+    fun close() {
+        clawServo.position = 1.0
+    }
+
+    /**
+     * Set wrist position.
+     * @param position new wrist position, between 0 (up) and 1 (down).
+     */
+    @JvmStatic @ExportToBlocks
+    fun setWrist(position: Double) {
+        wristServo.position = position
+    }
+}
