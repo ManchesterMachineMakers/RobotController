264dd0f5d1445ac5b491a1ee295936bd20247ee7
Aleks Rutins <aleks@rutins.com>
2022-12-30 18:27:27 -0500

    Add failing diagnostics test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticDiagnostic.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticDiagnostic.kt
@@ -4,9 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.rutins.aleks.diagonal.Subject
 import org.firstinspires.ftc.teamcode.diagnostics.DiagnosticsOpMode
 import org.firstinspires.ftc.teamcode.diagnostics.tests.DiagnosticTestingThingy
+import org.firstinspires.ftc.teamcode.diagnostics.tests.FailingDiagnosticTestingThingy
 import org.firstinspires.ftc.teamcode.diagnostics.tests.diagnosticTest
+import org.firstinspires.ftc.teamcode.diagnostics.tests.failingDiagnosticTest
 
 @TeleOp(name = "Diagnostic Diagnostic")
-class DiagnosticDiagnostic : DiagnosticsOpMode(::diagnosticTest) {
-    override fun provides(): Array<Subject> = arrayOf(DiagnosticTestingThingy())
+class DiagnosticDiagnostic : DiagnosticsOpMode(::diagnosticTest, ::failingDiagnosticTest) {
+    override fun provides(): Array<Subject> = arrayOf(DiagnosticTestingThingy(), FailingDiagnosticTestingThingy())
 }
