f5f43b3c986c086208628b68661a000725d78093
Aleks Rutins <aleks@rutins.com>
2023-02-04 14:29:10 -0500

    Add blinkin diagnostics opmode, remove diagnostic diagnostic from main diagnostics op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlinkinDiagnostic.kt
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode
+
+import org.firstinspires.ftc.teamcode.diagnostics.DiagnosticsOpMode
+import org.firstinspires.ftc.teamcode.diagnostics.tests.blinkinTest
+
+class BlinkinDiagnostic : DiagnosticsOpMode(::blinkinTest)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/diagnostics/DiagnosticsOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/diagnostics/DiagnosticsOpMode.kt
@@ -19,8 +19,7 @@ abstract class DiagnosticsOpMode(
         vararg val tests: KFunction1<DiagnosticsOpMode, Pair<Class<out Any>, (Array<Subject>, Runner) -> Test>> = arrayOf(
                 ::linearSlideTest,
                 ::pathfindingTest,
-                ::blinkinTest,
-                ::diagnosticTest
+                ::blinkinTest
         )
 ) : LinearOpMode() {
     open fun provides(): Array<Subject> = RobotConfig.allConnected().map { it }.toTypedArray()
