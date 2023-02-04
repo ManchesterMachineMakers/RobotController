90d959b7439ca224caa3795e52a03801480500c6
Aleks Rutins <aleks@rutins.com>
2023-02-04 14:31:17 -0500

    Annotate the blinkin diagnostic as a teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlinkinDiagnostic.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlinkinDiagnostic.kt
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.diagnostics.DiagnosticsOpMode
 import org.firstinspires.ftc.teamcode.diagnostics.tests.blinkinTest
 
+@TeleOp(name = "Blinkin Diagnostic", group = "Diagnostics")
 class BlinkinDiagnostic : DiagnosticsOpMode(::blinkinTest)
