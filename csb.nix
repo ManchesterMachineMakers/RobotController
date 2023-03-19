87aeee59683f2d89278b3081d26fee45361ccbac
Aleks Rutins <aleks@rutins.com>
2023-03-19 15:03:44 +0000

    Add android SDK

--- csb.nix
+++ csb.nix
@@ -1,10 +1,28 @@
 with import <nixpkgs> {};
 
-stdenv.mkDerivation {
+let
+    android-nixpkgs = callPackage (import (builtins.fetchGit {
+        url = "https://github.com/tadfisher/android-nixpkgs.git";
+    })) {
+        # Default; can also choose "beta", "preview", or "canary".
+        channel = "stable";
+    };
+    
+    android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
+        cmdline-tools-latest
+        build-tools-30-0-2
+        platform-tools
+        platforms-android-29
+        emulator
+    ]);
+    
+in stdenv.mkDerivation {
     name = "csb";
     buildInputs = [
         zulu
         kotlin
-        adoptopenjdk-hotspot-bin
+        kotlin-language-server
+        android-tools
+        android-sdk
     ];
 }
