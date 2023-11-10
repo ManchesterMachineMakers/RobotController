d02fa41a88163ef8e40486c20be201c52a5d0df9
Aleks Rūtiņš <aleks@rutins.com>
2023-11-10 15:23:28 +0000

    Fix stupid errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/contracts/Controllable.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/contracts/Controllable.kt
@@ -1,15 +1,14 @@
 package org.firstinspires.ftc.teamcode.contracts
 
 import com.qualcomm.robotcore.hardware.Gamepad
-import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import org.firstinspires.ftc.teamcode.util.GamepadManager
 
 // Controllable interface
 interface Controllable {
-    fun controller(gamepad: GamepadManager) 
+    fun controller(gamepad: GamepadManager)
 
     companion object {
-        fun runAll(vararg things: Pair<Gamepad, Controllable>)
-            = things.forEach { thing -> thing.1.controller(thing.0) }
+        fun runAll(vararg things: Pair<Gamepad, Controllable>) =
+                things.forEach { thing -> thing.second.controller(GamepadManager(thing.first)) }
     }
 }
