05e0aea4bd2169b07de8dd633fe571c43a48e947
Miles <mavernon42@gmail.com>
2023-11-30 19:26:28 -0500

    Split `SemiAutoArm.java` and `SemiAutoTeleOp.java` into continuous (ct) and incremented (inc). Also implemented non-functional code for incremented system.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/semiAutoArms/IncSemiAutoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/semiAutoArms/IncSemiAutoTeleOp.java
@@ -1,25 +1,25 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop;
+package org.firstinspires.ftc.teamcode.opmodes.teleop.semiAutoArms;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.teamcode.subassemblies.miles.Drivebase;
-import org.firstinspires.ftc.teamcode.subassemblies.miles.SemiAutoArm;
+import org.firstinspires.ftc.teamcode.subassemblies.miles.semiAuto.IncSemiAutoArm;
 
 // Comments courtesy of ChatGPT
-@TeleOp(name = "Semi-Auto TeleOp", group = "arm")
-public class SemiAutoTeleOp extends OpMode {
+@TeleOp(name = "Semi-Auto Inc Arm TeleOp", group = "arm")
+public class IncSemiAutoTeleOp extends OpMode {
 
     // Initializing robot subassemblies
     Drivebase drivebase = new Drivebase();
-    SemiAutoArm semiAutoArm = new SemiAutoArm();
+    IncSemiAutoArm incSemiAutoArm = new IncSemiAutoArm();
 
     @Override
     public void init() {
         // Update statuses
         drivebase.currentStatus = "initializing";
-        semiAutoArm.currentStatus = "initializing";
+        incSemiAutoArm.currentStatus = "initializing";
 
         // Setting up references for Drivebase
         drivebase.gamepad = gamepad1;
@@ -27,43 +27,43 @@ public class SemiAutoTeleOp extends OpMode {
         drivebase.hardwareMap = hardwareMap;
 
         // Setting up references for SemiAutoArm
-        semiAutoArm.gamepad = gamepad2;
-        semiAutoArm.telemetry = telemetry;
-        semiAutoArm.hardwareMap = hardwareMap;
+        incSemiAutoArm.gamepad = gamepad2;
+        incSemiAutoArm.telemetry = telemetry;
+        incSemiAutoArm.hardwareMap = hardwareMap;
 
         // Initializing subassemblies
         drivebase.init();
-        semiAutoArm.init();
+        incSemiAutoArm.init();
 
         // Update telemetry
         drivebase.telemetry();
-        semiAutoArm.telemetry();
+        incSemiAutoArm.telemetry();
     }
 
     @Override
     public void loop() {
         // Update statuses
         drivebase.currentStatus = "looping";
-        semiAutoArm.currentStatus = "looping";
+        incSemiAutoArm.currentStatus = "looping";
 
         // Update run time
         drivebase.runTime = time;
-        semiAutoArm.runTime = time;
+        incSemiAutoArm.runTime = time;
 
         // Safety check: Stops the robot if SemiAutoArm requests it
-        if (semiAutoArm.needsStop) {
+        if (incSemiAutoArm.needsStop) {
             requestOpModeStop();
-        } else if (semiAutoArm.arm.getCurrent(CurrentUnit.AMPS) > 10) {
+        } else if (incSemiAutoArm.arm.getCurrent(CurrentUnit.AMPS) > 10) {
             // Emergency stop if arm current exceeds 10 Amps
             terminateOpModeNow();
         }
 
         // Main loop functions for Drivebase and SemiAutoArm
         drivebase.loop();
-        semiAutoArm.loop();
+        incSemiAutoArm.loop();
 
         // Telemetry updates for monitoring
         drivebase.telemetry();
-        semiAutoArm.telemetry();
+        incSemiAutoArm.telemetry();
     }
 }
