30a592d953588785bb8080e390ebfad920bdeaa1
Miles <mavernon42@gmail.com>
2024-02-15 17:23:04 -0500

    Added a bunch of math angle conversions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/math.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/math.kt
@@ -1,11 +1,20 @@
 package org.firstinspires.ftc.teamcode.util
 
-import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
+import kotlin.math.*
 
 fun clamp(x: Double, lowerBound: Double, upperBound: Double)
     = max(lowerBound, min(upperBound, x))
 
 fun equalsTolerance(a: Double, b: Double, tolerance: Double)
     = abs(b - a) < tolerance
+
+fun encoderPositionToDegrees(encoderPosition: Int, encoderResolution: Double)
+    = 360 * encoderPosition / encoderResolution
+
+/** Assumes use of a 300 degree non-continuous servo */
+fun degreesToServoPosition(degrees: Double, scaleRange: Pair<Double, Double>) =
+    degrees / 300 * abs(scaleRange.second - scaleRange.first)
+
+fun toDegrees(radians: Double) = radians * Math.PI / 180
+
+fun toRadians(degrees: Double) = degrees * 180 / Math.PI
