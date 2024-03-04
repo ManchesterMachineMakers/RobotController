14b936d1322da23e5c23ea8eb7ade75b7d76d189
milesavernon <milesavernon@gmail.com>
2024-03-03 19:28:26 -0500

    updated DoNotBreakThisTeleOp.java to run at faster speeds using a different algorithm. Has been tested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DoNotBreakThisTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DoNotBreakThisTeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,53 +8,49 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name = "Do Not Break This TeleOp", group = "do not break")
 public class DoNotBreakThisTeleOp extends LinearOpMode {
 
-    
-
     @Override
     public void runOpMode() {
         // Initialize all motors
-        DcMotor leftFront  = hardwareMap.dcMotor.get("left_front");
+        DcMotor leftFront = hardwareMap.dcMotor.get("left_front");
         DcMotor rightFront = hardwareMap.dcMotor.get("right_front");
-        DcMotor leftRear   = hardwareMap.dcMotor.get("left_rear");
-        DcMotor rightRear  = hardwareMap.dcMotor.get("right_rear");
+        DcMotor leftRear = hardwareMap.dcMotor.get("left_rear");
+        DcMotor rightRear = hardwareMap.dcMotor.get("right_rear");
 
         // Yes, the code is repetitive, but it has better readability and is less work
-        configMotor(leftFront,  DcMotorSimple.Direction.REVERSE);
-        configMotor(rightFront, DcMotorSimple.Direction.FORWARD);
-        configMotor(leftRear,   DcMotorSimple.Direction.FORWARD);
-        configMotor(rightRear,  DcMotorSimple.Direction.REVERSE);
-
-        // Initialize here, rather than every loop, for greater efficiency
-        float rightX;
-        double  r,
-                robotAngle,
-                v1,
-                v2,
-                v3,
-                v4;
+        configMotor(leftFront, DcMotorSimple.Direction.FORWARD);
+        configMotor(rightFront, DcMotorSimple.Direction.REVERSE);
+        configMotor(leftRear, DcMotorSimple.Direction.REVERSE);
+        configMotor(rightRear, DcMotorSimple.Direction.FORWARD);
 
         waitForStart();
 
         if(opModeIsActive()) {
             while (opModeIsActive()) {
-                r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
-                robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
-                rightX = gamepad1.right_stick_x;
-                v1 = r * Math.cos(robotAngle) + rightX;
-                v2 = r * Math.sin(robotAngle) - rightX;
-                v3 = r * Math.sin(robotAngle) + rightX;
-                v4 = r * Math.cos(robotAngle) - rightX;
-                leftFront.setPower(v1 / 1.2);
-                rightFront.setPower(v2 / 1.2);
-                leftRear.setPower(v3 / 1.2);
-                rightRear.setPower(v4 / 1.2);
+                // from https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+                double leftX = gamepad1.left_stick_x;
+                double leftY = gamepad1.left_stick_y;
+                double rightX = -gamepad1.right_stick_x;
+
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio,
+                // but only if at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(leftY) + Math.abs(leftX) + Math.abs(rightX), 1);
+                double leftFrontPower = (leftY + leftX + rightX) / denominator;
+                double rightFrontPower = (leftY - leftX - rightX) / denominator;
+                double leftRearPower = (leftY - leftX + rightX) / denominator;
+                double rightRearPower = (leftY + leftX - rightX) / denominator;
+
+                leftFront.setPower(leftFrontPower);
+                rightFront.setPower(rightFrontPower);
+                leftRear.setPower(leftRearPower);
+                rightRear.setPower(rightRearPower);
             }
         }
     }
 
     // Configures input motor to inputted direction
     public void configMotor(DcMotor motor, DcMotorSimple.Direction direction) {
-        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Fine for TeleOp unless Semi-Auto
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Fine for TeleOp
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motor.setDirection(direction);
     }
