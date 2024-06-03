083a7e6ee558dd20f96a1a5920bbc9f89b4cba63
Miles <mavernon42@gmail.com>
2024-06-02 20:02:14 -0400

    Actual Camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dashboard/DashCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dashboard/DashCamera.java
@@ -1,42 +1,69 @@
-package org.firstinspires.ftc.teamcode.dashboard;
+// IMPORTANT: I pulled this from https://github.com/ErikOverflow/FtcRobotController/blob/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcDashboard_Camera.java
 
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subassemblies.Arm;
-import org.firstinspires.ftc.teamcode.subassemblies.DriveBase;
-import org.firstinspires.ftc.teamcode.subassemblies.DroneLauncher;
-import org.firstinspires.ftc.teamcode.subassemblies.PixelReleases;
-import org.firstinspires.ftc.teamcode.subassemblies.Winch;
+import org.firstinspires.ftc.robotcore.external.function.Consumer;
+import org.firstinspires.ftc.robotcore.external.function.Continuation;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
 
-@Config
-@TeleOp(name="Dash Camera", group="Dashboard")
+import java.util.concurrent.atomic.AtomicReference;
+
+@Autonomous(name="Dash Camera", group="Linear")
 public class DashCamera extends LinearOpMode {
+    //Talk Volatile vs AtomicReference, final to make sure it is only initialized once, etc.
+    public static class CameraStreamProcessor implements VisionProcessor, CameraStreamSource{
+        private final AtomicReference<Bitmap> lastFrame = new AtomicReference<>(Bitmap.createBitmap(1,1, Bitmap.Config.RGB_565));
+        //private volatile Bitmap lastFrame =  Bitmap.createBitmap(1,1, Bitmap.Config.RGB_565);
+        @Override
+        public void getFrameBitmap(Continuation<? extends Consumer<Bitmap>> continuation) {
+            continuation.dispatch(bitmapConsumer -> bitmapConsumer.accept(lastFrame.get()));
+        }
 
+        @Override
+        public void init(int width, int height, CameraCalibration cameraCalibration) {
+            lastFrame.set(Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565));
+        }
+
+        @Override
+        public Object processFrame(Mat frame, long captureTimeNanos) {
+            Bitmap b = Bitmap.createBitmap(frame.width(), frame.height(), Bitmap.Config.RGB_565);
+            Utils.matToBitmap(frame, b);
+            lastFrame.set(b);
+            return null;
+        }
+
+        @Override
+        public void onDrawFrame(Canvas canvas, int i, int i1, float v, float v1, Object o) {
+
+        }
+    }
     @Override
     public void runOpMode() throws InterruptedException {
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        final CameraStreamProcessor processor = new CameraStreamProcessor();
 
-        DriveBase driveBase = new DriveBase(this);
-        Arm arm = new Arm(this);
-        DroneLauncher droneLauncher = new DroneLauncher(this);
-        PixelReleases pixelReleases = new PixelReleases(this);
-        Winch winch = new Winch(this);
-
+        new VisionPortal.Builder().
+                addProcessor(processor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .build();
         waitForStart();
 
-        if(opModeIsActive())
+        FtcDashboard.getInstance().startCameraStream(processor, 0);
 
-            while(opModeIsActive()) {
-                driveBase.control(gamepad1);
-                arm.control(gamepad2);
-                droneLauncher.control(gamepad2.x);
-                pixelReleases.control(gamepad2);
-                winch.control(gamepad2.right_stick_y);
-            }
+        while(opModeIsActive()){
+            sleep(100);
+        }
     }
 }
