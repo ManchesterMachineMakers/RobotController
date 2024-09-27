6c7c2b3b8feab99d6b857d2fc5c1a5e581e44f80
Miles Vernon <milesavernon@gmail.com>
2024-09-27 15:54:59 -0400

    NOTE: farthergate was not working, so I disabled all reference and nothings seems affected.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/visionTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/visionTest.kt
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.tests
-
-import com.rutins.aleks.diagonal.describe
-import org.firstinspires.ftc.teamcode.opmodes.diagnostics.DiagnosticsOpMode
-import org.firstinspires.ftc.teamcode.subassemblies.Vision
-
-fun visionTest(opMode: DiagnosticsOpMode) = describe<Vision> { vision ->
-    it("initializes") {
-        (vision.aprilTag != null && vision.visionPortal != null).expect("Vision failed to initialize")
-    }
-}
+//package org.firstinspires.ftc.teamcode.tests
+//
+//import com.rutins.aleks.diagonal.describe
+//import org.firstinspires.ftc.teamcode.opmodes.diagnostics.DiagnosticsOpMode
+//import org.firstinspires.ftc.teamcode.subassemblies.Vision
+//
+//fun visionTest(opMode: DiagnosticsOpMode) = describe<Vision> { vision ->
+//    it("initializes") {
+//        (vision.aprilTag != null && vision.visionPortal != null).expect("Vision failed to initialize")
+//    }
+//}
