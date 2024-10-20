e50f187b9760a82b11bdc01e10ecc17f54bbdce4
Miles Vernon <149839790+MilesVernon@users.noreply.github.com>
2024-10-20 13:56:08 -0400

    directionality fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassemblies/DriveBase.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassemblies/DriveBase.kt
@@ -35,10 +35,10 @@ class DriveBase(opMode: LinearOpMode) : Subassembly(opMode, "Drive Base") {
 
     init {
         // direction = FORWARD by default
-        //leftFront.direction = DcMotorSimple.Direction.REVERSE
-        rightFront.direction = DcMotorSimple.Direction.REVERSE
+        leftFront.direction = DcMotorSimple.Direction.REVERSE
+//        rightFront.direction = DcMotorSimple.Direction.REVERSE
         leftRear.direction = DcMotorSimple.Direction.REVERSE
-        //rightRear.direction = DcMotorSimple.Direction.REVERSE
+//        rightRear.direction = DcMotorSimple.Direction.REVERSE
 
         opMode.log("DriveBase successfully initialized")
     }
